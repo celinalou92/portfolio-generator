2046dfd45f8cb8d1fab663f0a895323cb03eb3f3
Celina Louissaint <celinalouissaint@gmail.com>
2021-04-08 17:27:43 -0700

    generate website

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // TODO Pick back up on 9.4.3
 
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+
 
 
 
@@ -171,22 +172,39 @@ const promptProject = portfolioData => {
     ]
   };
   // test funtionality
-  const pageHTML = generatePage(mockData);
-  fs.writeFile('./index.html', pageHTML, err => {if (err) throw err});
+  // const pageHTML = generatePage(mockData);
+  // fs.writeFile('./dist/index.html', pageHTML, err => {
+  //   if (err) {
+  //     console.log(err);
+  //     return
+  //   }
+  //   console.log('Page created! Check out index.html in this directory to see it!');
+  //   fs.copyFile('./src/style.css','./dist/style.css', err =>{
+  //     if(err) {
+  //       console.log(err);
+  //       return;
+  //     }
+  //     console.log('Style sheet copied successfully!')
+  //   })
+  // })
 
-
-// promptUser()
-//     .then(promptProject)
-//     .then(portfolioData => {
-//       const pageHTML = generatePage(portfolioData);
-    
-//       fs.writeFile('./index.html', pageHTML, err => {
-//         if (err) throw err;
-
-//         console.log('Portfolio complete! Check out index.html to see the output!');
-//       });
-//     });
-
+promptUser()  
+.then(promptProject)
+.then(portfolioData => {
+  return generatePage(portfolioData);
+})
+.then(pageHTML => {
+  return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+  console.log(writeFileResponse);
+})
+.then(copyFileResponse => {
+  console.log(copyFileResponse);
+})
+.catch(err => {
+  console.log(err);
+})
 
     
 
